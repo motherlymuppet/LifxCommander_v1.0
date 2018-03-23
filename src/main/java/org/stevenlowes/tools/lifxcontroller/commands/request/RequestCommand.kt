bed84001ee42c406ed6b7e8af4c770d8c02cc259
Steven Lowes <motherlymuppet@users.noreply.github.com>
2018-03-23 20:22:42 +0000

    Remove unused methods

--- src/main/java/org/stevenlowes/tools/lifxcontroller/commands/request/RequestCommand.kt
+++ src/main/java/org/stevenlowes/tools/lifxcontroller/commands/request/RequestCommand.kt
@@ -6,9 +6,7 @@ import org.stevenlowes.tools.lifxcontroller.commands.header.FrameAddress
 import org.stevenlowes.tools.lifxcontroller.commands.header.Protocol
 import org.stevenlowes.tools.lifxcontroller.values.Net
 import java.net.DatagramPacket
-import java.net.DatagramSocket
 import java.net.InetAddress
-import java.net.NetworkInterface
 
 abstract class RequestCommand(code: Int) : ProtocolType(code), Command {
     abstract val payloadBytes: ByteArray
@@ -41,29 +39,6 @@ abstract class RequestCommand(code: Int) : ProtocolType(code), Command {
             byteArray[i] = this.payloadBytes[i - protocolEnd]
         }
         byteArray
-    }
-
-    override fun broadcast() {
-        //TODO keep sendSocket open
-        val socket = DatagramSocket()
-        socket.broadcast = true
-
-        val interfaces = NetworkInterface.getNetworkInterfaces()
-        while (interfaces.hasMoreElements()) {
-            val networkInterface = interfaces.nextElement()
-
-            if (networkInterface.isLoopback || !networkInterface.isUp) {
-                continue
-            }
-
-            for (interfaceAddress in networkInterface.interfaceAddresses) {
-                val broadcast = interfaceAddress.broadcast ?: continue
-
-                val packet = DatagramPacket(requestBytes, requestBytes.size, broadcast, Net.port)
-                socket.send(packet)
-            }
-        }
-        socket.close()
     }
 
     override fun send(lights: List<InetAddress>) {
--- src/main/java/org/stevenlowes/tools/lifxcontroller/controller/ControlMethods.kt
+++ src/main/java/org/stevenlowes/tools/lifxcontroller/controller/ControlMethods.kt
@@ -4,38 +4,7 @@ import java.net.DatagramPacket
 import java.net.DatagramSocket
 import java.net.InetAddress
 import java.net.NetworkInterface
-//TODO get rid of tihs
 internal object ControlMethods {
-    fun sendBroadcastMessage(messageByteArray: ByteArray, port: Int) {
-        val socket = DatagramSocket()
-        socket.broadcast = true
-
-        val interfaces = NetworkInterface.getNetworkInterfaces()
-        while (interfaces.hasMoreElements()) {
-            val networkInterface = interfaces.nextElement()
-
-            if (networkInterface.isLoopback || !networkInterface.isUp) {
-                continue
-            }
-
-            for (interfaceAddress in networkInterface.interfaceAddresses) {
-                val broadcast = interfaceAddress.broadcast ?: continue
-
-                val packet = DatagramPacket(messageByteArray, messageByteArray.size, broadcast, port)
-                socket.send(packet)
-            }
-        }
-        socket.close()
-    }
-
-    fun sendUdpMessage(messageByteArray: ByteArray, ipAddress: String, port: Int) {
-        val address = InetAddress.getByName(ipAddress)
-        val packet = DatagramPacket(messageByteArray, messageByteArray.size, address, port)
-        val socket = DatagramSocket()
-        socket.send(packet)
-        socket.close()
-    }
-
     fun receiveUdpMessage(port: Int): ByteArray {
         val socket = DatagramSocket(port)
         val data = ByteArray(1500)
