c4d626788cc1c1288918c80bc40e3e128187b72d
Steven Lowes <motherlymuppet@users.noreply.github.com>
2018-10-09 16:49:43 +0100

    Missed another implementation

--- src/main/java/org/stevenlowes/tools/lifxcontroller/commands/CompoundCommand.kt
+++ src/main/java/org/stevenlowes/tools/lifxcontroller/commands/CompoundCommand.kt
@@ -4,7 +4,7 @@ import org.stevenlowes.tools.lifxcontroller.commands.request.Command
 import java.net.InetAddress
 
 open class CompoundCommand(vararg val commands: Command): Command{
-    override fun send(lights: List<InetAddress>) {
+    override fun send(lights: Collection<InetAddress>) {
         for (command in commands) {
             command.send(lights)
         }
