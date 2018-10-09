a2e263f3f6f92eaab18dfca0be51c4e2a091ac4d
Steven Lowes <motherlymuppet@users.noreply.github.com>
2018-10-09 16:43:47 +0100

    Switch from list to collection to allow more flexibility

--- src/main/java/org/stevenlowes/tools/lifxcontroller/commands/request/Command.kt
+++ src/main/java/org/stevenlowes/tools/lifxcontroller/commands/request/Command.kt
@@ -3,7 +3,7 @@ package org.stevenlowes.tools.lifxcontroller.commands.request
 import java.net.InetAddress
 
 interface Command{
-    fun send(lights: List<InetAddress>)
+    fun send(lights: Collection<InetAddress>)
 
     fun send(light: InetAddress){
         send(listOf(light))
